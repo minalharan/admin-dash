84dd0e6ffa22dc6bbdbd21f17beb1a540859def1
hariomnagriya <hariom.chapter247@gmail.com>
2019-05-22 18:48:04 +0530

    delete user page update

--- src/views/Users/Users.js
+++ src/views/Users/Users.js
@@ -1,31 +1,81 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Badge, Card, CardBody, CardHeader, Col, Row, Table } from 'reactstrap';
+import {Image,Button} from 'react-bootstrap';
+import Swal from 'sweetalert2'
 import axios from "axios";
+const BASE_URL="http://192.168.2.112:8080/";
 //import usersData from './UsersData'
 
-function UserRow(props) {
-  const user = props.user
-  const userLink = `/users/${user._id}`
-
-  const getBadge = (status) => {
+class UserRow extends Component {
+  //const user = props.user
+ //const userLink = `/users/${user._id}`
+  
+   getBadge = (status) => {
     return status === 'Active' ? 'success' :
       status === 'Inactive' ? 'secondary' :
         status === 'Pending' ? 'warning' :
           status === 'Banned' ? 'danger' :
             'primary'
   }
+  onSubmit = async e => {
+    e.preventDefault();
+    try {
+      const token = localStorage.getItem("token");
+      const response = await axios.delete(
+        "http://192.168.2.107:8080/deleteUser/" + this.props.obj._id
+      );
+      this.props.history.push("/product-list");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+ 
+render(){
 
   return (
-    <tr key={user._id.toString()}>
-      <th scope="row"><Link to={userLink}>{props.index+1}</Link></th>
-      <td><Link to={userLink}>{user.name}</Link></td>
-      <td>{user.email}</td>
-      <td>{user.mobile_no}</td>
-      <td>{user.gender}</td>
-      <td><Link to={userLink}><Badge color={getBadge(user.status)}>{user.status}</Badge></Link></td>
+    <>
+      {/* key={user._id.toString()} */}
+    <tr >
+      <th scope="row"><Link to={"/users/"+this.props.user._id}>{this.props.index+1}</Link></th>
+      <th><Image src={BASE_URL+this.props.user.file} width="80px" height="80px"/></th>
+      <td><Link to={"/users/"+this.props.user._id}>{this.props.user.name}</Link></td>
+      <td>{this.props.user.email}</td>
+      <td>{this.props.user.mobile_no}</td>
+      <td>{this.props.user.gender}</td>
+      <td><Link to={"/users/"+this.props.user._id}><Badge color={this.getBadge(this.props.user.status)}>{this.props.user.status}</Badge></Link></td>
+      <td colSpan="3"> 
+      <Link to={"/users/" + this.props.user._id}>
+        <Button variant="outline-primary"><i class="fas fa-edit"></i></Button>
+      </Link>&nbsp;
+        <Button variant="outline-success"><i class="fas fa-eye"></i></Button>
+        <Button variant="outline-danger"
+                onClick={e =>
+                  Swal.fire({
+                    title: 'Are you sure?',
+                    text: "You won't be able to delete this!",
+                    type: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes, delete it!'
+                  }).then((result) => {
+                    if (result.value) {
+                      Swal.fire(
+                        'Deleted!',
+                        'Your file has been deleted.',
+                        'success'
+                      ) && this.props.onDelete(this.props.obj._id)
+                    }
+                  })
+
+                }
+              ><i class="fas fa-trash-alt"></i></Button>
+      </td>
     </tr>
+    </>
   )
+              }
 }
 
 class Users extends Component {
@@ -34,17 +84,32 @@ class Users extends Component {
   this.state = { user: []
   }
   }
-  componentDidMount = async () => {  
-    //const{user}=this.state;
-    const res = await axios.get("http://192.168.2.112:8080/getuser")//console.log(res.data.result);
-    //console.log("result");
-    const result = res.data.result1;
+  componentDidMount = async () => {
+    this.getData();
+      }
+      
+     getData=async()=>{
+      
+    const response =await axios.get("http://192.168.2.107:8080/getuser");
+
+    const result = response.data.result1;
     this.setState({ user: result});
-   // console.log("users :-",result)
     if (!result) {
     console.log("error");
     }
     }
+    onDelete= async productId=>{
+    
+      try {
+      const response = await axios.delete(
+      "http://192.168.2.107:8080/deleteUser/" + productId )
+      } catch (error) {
+      console.log(error);
+      }
+      this.getData();
+    }
+    
+    
     
   render() {
 
@@ -66,17 +131,19 @@ class Users extends Component {
                   <thead>
                     <tr>
                       <th scope="col">S.No.</th>
+                      <th scope="col">Image</th>
                       <th scope="col">Name</th>
                       <th scope="col">Email</th>
                       <th scope="col">Mobile</th>
                       <th scope="col">Gender</th>
                       <th scope="col">status</th>
+                      <th scope ="col" colSpan="3">Actions</th>
                     </tr>
                   </thead>
                   <tbody>
-                    {console.log("uuuuuu  :-",user.length)}
+                   
                     {user && user.length ? user.map((user, index) =>
-                      <UserRow obj={user} key={user._id} user={user} index={index} />)
+                      <UserRow obj={user} key={user._id} user={user} index={index} onDelete={this.onDelete}/>)
                       : null}
                       
                     
